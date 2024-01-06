001fb7ad0ae068582396190994bdf8310aa1ac81
AnnaCrystals <ddvbeheer@gmail.com>
2024-01-06 21:13:51 +0100

    Update

--- game.cpp
+++ game.cpp
@@ -2,8 +2,6 @@
 #include "surface.h"
 #include <cstdio> //printf
 #include <iostream>
-#include "game.h"
-#include "surface.h"
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <SDL_scancode.h>
@@ -12,15 +10,15 @@
 namespace Tmpl8
 {
 
-
 	class Bottle
 	{
 	public:
 		Bottle()
 		{
-			bottlePosition.x = ScreenWidth / 2.0f;
-			bottlePosition.y = ScreenHeight - 300.0f;
+			bottlePosition.x = ScreenWidth / 2.0f + 100.0f;
+			bottlePosition.y = ScreenHeight - 50.0f;
 			bottleSprite = new Sprite(new Surface("assets/Pot21.png"), 1);
+
 		}
 
 		Sprite* bottleSprite;
@@ -28,28 +26,38 @@ namespace Tmpl8
 		const int bottleHeight = 50.0f;
 		const int bottleWidth = 50.0f;
 
-		void Bottle::Spawn(Surface* gameScreen, vec2& ballPosition, bool& hitObject, bool&disableSpawn)
+
+		void Spawn(Surface* gameScreen, vec2& ballPosition)
+		{
+			bottleSprite->Draw(gameScreen, static_cast<int>(bottlePosition.x), static_cast<int>(bottlePosition.y));
+		}
+
+		void DeSpawn(int number)
 		{
 
-			int bottleMinX = static_cast<int>(ballPosition.x / bottleWidth);
-			int bottleMinY = static_cast<int>(ballPosition.y / bottleHeight);
-			int bottleMaxX = static_cast<int>((ballPosition.x + 50) / bottleWidth);
-			int bottleMaxY = static_cast<int>((ballPosition.y + 50) / bottleHeight);
-
-
-
-
-			//if (hitSpawn && enabledSpawn) {
-			if (!hitObject && !disableSpawn) {
-				//if (map[bottleMinY][bottleMinX * 3 + 2] == 'S' && map[bottleMaxY][bottleMinX * 3 + 2] != 'S') {
-				bottleSprite->Draw(gameScreen, static_cast<int> (bottlePosition.x), static_cast<int>(bottlePosition.y));
-				//}
+			if (number == 1) {
+				delete bottleSprite;
 			}
+
+			/*if (number == 2) {
+				delete bottleSprite;
+			}
+
+			if (number == 3) {
+				delete bottleSprite;
+			}
+
+			if (number == 4) {
+				delete bottleSprite;
+			}
+
+			if (number == 5) {
+				delete bottleSprite;
+			}*/
+
 		}
-		
+
 	};
-
-
 
 
 	void Game::Init()
@@ -59,15 +67,12 @@ namespace Tmpl8
 		ballSize.y = ballSprite->GetHeight() / 2.0f;
 		ballPosition.x = ScreenWidth / 2.0f;
 		ballPosition.y = ScreenHeight - ballSprite->GetHeight();
-
-		/*bottlePosition.x = ScreenWidth / 2.0f;
-		bottlePosition.y = ScreenHeight - 300.0f;
-		bottleSprite = new Sprite(new Surface("assets/Pot21.png"), 1);*/
-		
 	}
+
+
 	void Game::Shutdown() {}
 
-	//The tilemap
+
 	Surface tiles("assets/mediaval.png");
 	const int numRows = 16;
 	const int numColums = 76;
@@ -79,45 +84,55 @@ namespace Tmpl8
 		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
 		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
 		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
-		 "lc lcSlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
-		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lcSlc lc lc",
-		 "abXabXabXabXlc lc lc lc lc lc lc lc lcSlc lc lc lc lc lc lc abXabXabXabXab",
-		 "lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXlc lc lc lc lc abXabXabXabXab",
+		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
+		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
+		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXabX",
+		 "lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXlc lc lc lc lc abXabXabXabXabX",
 		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
 		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
-		 "lc lcSlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lcSlc lc lc lc",
-		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXab",
-		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXab",
+		 "lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc",
+		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXabX",
+		 "abXabXabXabXlc lc lc lc lc lc lc lc lc lc lc lc lc lc lc lc abXabXabXabXabX",
 	};
-
-	
-
 
 
 	//Drawing of a singular tile
 	void DrawTile(int tx, int ty, Surface* screen, int x, int y)
 	{
 		//Pixel* src = tiles.GetBuffer() + 1 + tx * 33 + (1 + ty * 33) * 595;
-		Pixel* src = tiles.GetBuffer()  + tx * 33 + (ty * 33) * 595;
+		Pixel* src = tiles.GetBuffer() + tx * 33 + (ty * 33) * 595;
 		Pixel* dst = screen->GetBuffer() + x + y * 800;
 		for (int i = 0; i < 32; i++, src += tiles.GetWidth(), dst += 800)
 			for (int j = 0; j < 32; j++)
 				dst[j] = src[j];
 	}
 
-	int score = 0;
-	bool disableSpawn = false;
+	static bool AABB(int aX, int aY, int aWidth, int aHeight, int bX, int bY, int bWidth, int bHeight)
+	{
+		return aX < bX + bWidth &&
+			aX + aWidth >= bX &&
+			aY <= bY + bHeight &&
+			aY + aHeight >= bY;
+	}
+
+
+
+
+	int number;
+
 	void Game::Tick(float deltaTime)
 	{
 		Bottle myBottle;
 		bool hitObject = false;
-		
+
 		deltaTime /= 1000.0f;
-		
+
 
 		prevBallPosition.y = ballPosition.y;
 		prevBallPosition.x = ballPosition.x;
 		prevGravity = gravity;
+
+
 
 		if (GetAsyncKeyState(VK_SPACE)) ballPosition.y = ballPosition.y - 10.0f;
 		if (GetAsyncKeyState(VK_LEFT)) ballPosition.x--;
@@ -125,8 +140,12 @@ namespace Tmpl8
 		if (GetAsyncKeyState(VK_DOWN)) ballPosition.y++;
 		if (GetAsyncKeyState(VK_UP)) ballPosition.y--;
 
-		ballPosition.y += ballVelocity.y * gravity *  deltaTime;
+
+
+		ballPosition.y += ballVelocity.y * gravity * deltaTime;
 		ballPosition.x += ballVelocity.x * gravity * deltaTime;
+
+
 
 		// Apply gravity
 		if (ballPosition.y + ballSprite->GetHeight() < screen->GetHeight()) {
@@ -134,41 +153,51 @@ namespace Tmpl8
 			gravity += 1.5f;
 		}
 
+
+
 		int tileMinX = static_cast<int>(ballPosition.x / tileWidth);
 		int tileMinY = static_cast<int>(ballPosition.y / tileHeight);
 		int tileMaxX = static_cast<int>((ballPosition.x + 50) / tileWidth);
 		int tileMaxY = static_cast<int>((ballPosition.y + 50) / tileHeight);
-
+		int tileNew = static_cast<int>((ballPosition.y + 49) / tileHeight);
 		// Check for collisions and reset position if necessary
 		if (map[tileMinY][tileMinX * 3 + 2] == 'X' && map[tileMaxY][tileMinX * 3 + 2] != 'X') {
 			ballPosition.x = prevBallPosition.x;
 			ballPosition.y = prevBallPosition.y;
-			gravity = 0.0f;
+			//gravity = 0.0f;
 			//gravity = prevGravity;
 		}
-
 		if (map[tileMinY][tileMaxX * 3 + 2] == 'X' && map[tileMaxY][tileMaxX * 3 + 2] != 'X') {
 			ballPosition.x = prevBallPosition.x;
 			ballPosition.y = prevBallPosition.y;
+			//gravity = 0.0f;
+			//gravity = prevGravity;
+		}
+		if (map[tileMaxY][tileMaxX * 3 + 2] == 'X' && map[tileNew][tileMaxX * 3 + 2] != 'X') {
+			ballPosition.x = prevBallPosition.x;
+			ballPosition.y = prevBallPosition.y - 1.0f;
+			gravity = 0.0f;
+			//gravity = prevGravity;
+		}
+		if (map[tileMaxY][tileMinX * 3 + 2] == 'X' && map[tileNew][tileMinX * 3 + 2] != 'X') {
+			ballPosition.x = prevBallPosition.x;
+			ballPosition.y = prevBallPosition.y - 1.0f;
 			gravity = 0.0f;
 			//gravity = prevGravity;
 		}
 
-		if (map[tileMaxY][tileMaxX * 3 + 2] == 'X') {
+		if (map[tileMaxY][tileMaxX * 3 + 2] == 'X' && map[tileNew][tileMaxX * 3 + 2] == 'X') {
 			ballPosition.x = prevBallPosition.x;
 			ballPosition.y = prevBallPosition.y;
 			gravity = 0.0f;
 			//gravity = prevGravity;
 		}
-
-
-		if (map[tileMaxY][tileMinX * 3 + 2] == 'X') {
+		if (map[tileMaxY][tileMinX * 3 + 2] == 'X' && map[tileNew][tileMinX * 3 + 2] == 'X') {
 			ballPosition.x = prevBallPosition.x;
 			ballPosition.y = prevBallPosition.y;
 			gravity = 0.0f;
 			//gravity = prevGravity;
 		}
-
 		if (ballPosition.y + ballSprite->GetHeight() > screen->GetHeight())
 		{
 			ballPosition.y = screen->GetHeight() - ballSprite->GetHeight();
@@ -178,15 +207,58 @@ namespace Tmpl8
 
 
 
-		if (map[tileMinY][tileMinX * 3 + 2] == 'S' || 
-			map[tileMaxY][tileMinX * 3 + 2] == 'S' || 
-			map[tileMinY][tileMaxX * 3 + 2] == 'S' || 
-			map[tileMaxY][tileMaxX * 3 + 2] == 'S') {
-			hitObject = true;
-			disableSpawn = true;
-			//score++;
-			//std::cout << score << std::endl;
+
+
+
+
+
+		float positionOneX = ScreenWidth / 2.0f + 100.0f;
+		float positionOneY = ScreenHeight - ballSprite->GetHeight();
+
+		float positionTwoX = ScreenWidth / 2.0f - 100.0f;
+		float positionTwoY = ScreenHeight - ballSprite->GetHeight();
+
+		float positionThreeX = ScreenWidth / 2.0f + 200.0f;
+		float positionThreeY = ScreenHeight - ballSprite->GetHeight();
+
+		float positionFourX = ScreenWidth / 2.0f - 200.0f;
+		float positionFourY = ScreenHeight - ballSprite->GetHeight();
+
+		float positionFiveX = ScreenWidth / 2.0f + 300.0f;
+		float positionFiveY = ScreenHeight - ballSprite->GetHeight();
+
+		if (AABB(ballPosition.x, ballPosition.y, 50, 50,
+			positionOneX, positionOneY, 50, 50))
+		{
+			number = 1;
+			myBottle.DeSpawn(number);
 		}
+		if (AABB(ballPosition.x, ballPosition.y, 50, 50,
+			positionTwoX, positionTwoY, 50, 50))
+		{
+			number = 2;
+			myBottle.DeSpawn(number);
+		}
+		if (AABB(ballPosition.x, ballPosition.y, 50, 50,
+			positionThreeX, positionThreeY, 50, 50))
+		{
+			number = 3;
+			myBottle.DeSpawn(number);
+		}
+		if (AABB(ballPosition.x, ballPosition.y, 50, 50,
+			positionFourX, positionFourY, 50, 50))
+		{
+			number = 4;
+			myBottle.DeSpawn(number);
+		}
+		if (AABB(ballPosition.x, ballPosition.y, 50, 50,
+			positionFiveX, positionFiveY, 50, 50))
+		{
+			number = 5;
+			myBottle.DeSpawn(number);
+		}
+
+
 
 
 		screen->Clear(0);
@@ -203,10 +275,9 @@ namespace Tmpl8
 			}
 		}
 
-		myBottle.Spawn(screen, ballPosition, hitObject, disableSpawn);
-		//bottleSprite->Draw(screen, static_cast<int> (bottlePosition.x), static_cast<int>(bottlePosition.y));
-		ballSprite->Draw(screen, static_cast<int> (ballPosition.x), static_cast<int>(ballPosition.y));
 
+		myBottle.Spawn(screen, ballPosition);
+		ballSprite->Draw(screen, static_cast<int> (ballPosition.x), static_cast<int>(ballPosition.y));
 		screen->Box(ballPosition.x, ballPosition.y, ballPosition.x + ballSprite->GetWidth(), ballPosition.y + ballSprite->GetHeight(), 0xffff00);
 	}
 };
